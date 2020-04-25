628739458b80a6da8afb16db26359c9cc9a04d60
morrillo <morrillo@umich.edu>
2020-04-25 17:46:34 +0000

    Primer commit de migiacion a la versión 13

---
+++ models/account_move.py
@@ -0,0 +1,14 @@
+# © 2016 Carlos Dauden <carlos.dauden@tecnativa.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import fields, models
+
+
+class AccountMove(models.Model):
+    _inherit = 'account.move'
+
+    # We keep this field for migration purpose
+    contract_id = fields.Many2one(
+        'contract.contract', string='Contrato')
+    old_contract_id = fields.Many2one(
+        'contract.contract', oldname='contract_id')
